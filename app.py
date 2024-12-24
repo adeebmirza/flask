5e978e53b5b87bbbf92f129bdbf72496cd233aaf
adeebmirza <devilmirzaa3@gmail.com>
2024-12-24 20:35:10 +0530

    first commit 2

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create the Flask application instance
+app = Flask(__name__)
+
+# Define a route and its corresponding request handler
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+# Run the Flask development server
+if __name__ == '__main__':
+    app.run(host='0.0.0.0' ,debug=True,port=5000)
